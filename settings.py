5d22a8bb260acac6655ef0ed3782c9933f8bd468
Andrew Maddaford <ajmaddaford@gmail.com>
2016-06-16 17:18:11 +0100

    Optimistically queue a notification after saving the response

--- settings.py
+++ settings.py
@@ -9,3 +9,13 @@ LOGGING_LEVEL = logging.DEBUG
 
 # Default to true, cast to boolean
 MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+
+RABBIT_QUEUE = os.getenv('RABBITMQ_QUEUE', 'survey-notifications')
+
+RABBIT_URL = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
+    hostname=os.getenv('RABBITMQ_HOST', 'rabbit'),
+    port=os.getenv('RABBITMQ_PORT', 5672),
+    user=os.getenv('RABBITMQ_DEFAULT_USER', 'rabbit'),
+    password=os.getenv('RABBITMQ_DEFAULT_PASS', 'rabbit'),
+    vhost=os.getenv('RABBITMQ_DEFAULT_VHOST', '%2f')
+)
