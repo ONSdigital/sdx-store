be7b519cc568a8e6bbc15f441472ceb69029949f
Elliott Jenkins <elliott.jenkins@methods.co.uk>
2016-07-08 14:21:45 +0100

    testing wip

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,6 +1,20 @@
 import unittest
+from server import app
 
 
 class TestStoreService(unittest.TestCase):
+
+    endpoint = "/responses"
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def test_empty_post_request(self):
+        r = self.app.post(self.endpoint)
+        self.assertEqual(400, r.status_code)
+
     def test_run(self):
         self.assertEqual(True, True)
