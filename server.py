ae1333c75f4e0db63d695d28ec5835b6c395d3dc
Jenn <jennifer.edwards@methods.co.uk>
2016-07-08 11:23:48 +0100

    added accidental removal

--- server.py
+++ server.py
@@ -173,4 +173,3 @@ if __name__ == '__main__':
     logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
     port = int(os.getenv("PORT"))
     app.run(debug=True, host='0.0.0.0', port=port)
-
