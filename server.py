84d98c60510827eb87e21dc96d73607c4f000981
Jenn <jennifer.edwards@methods.co.uk>
2016-07-14 16:39:10 +0100

    standards changes and queueing added

--- server.py
+++ server.py
@@ -1,11 +1,7 @@
 import settings
-import json
-import html
 import logging
 import logging.handlers
-import requests
-import os
-from flask import Flask, request, Response, jsonify, abort
+from flask import Flask, request, jsonify
 from pymongo import MongoClient
 import pymongo.errors
 from datetime import datetime
@@ -13,64 +9,127 @@ from voluptuous import Schema, Coerce, All, Range, MultipleInvalid
 from bson.objectid import ObjectId
 from bson.errors import InvalidId
 import pika
+from structlog import wrap_logger
 
+logger = wrap_logger(logging.getLogger(__name__))
 app = Flask(__name__)
-
 app.config['MONGODB_URL'] = settings.MONGODB_URL
-logger = settings.logger
 
-mongo_client = MongoClient(app.config['MONGODB_URL'])
-db = mongo_client.sdx_store
+schema = Schema({
+    'survey_id': str,
+    'form': str,
+    'ru_ref': str,
+    'period': str,
+    'added_ms': Coerce(int),
+    'per_page': All(Coerce(int), Range(min=1, max=100)),
+    'page': All(Coerce(int), Range(min=1))
+})
 
-def queue_notification(notification):
-    logger.debug(" [x] Queuing notification to " + settings.RABBIT_QUEUE)
-    logger.debug(notification)
-    connection = pika.BlockingConnection(pika.URLParameters(settings.RABBIT_URL))
-    channel = connection.channel()
-    channel.queue_declare(queue=settings.RABBIT_QUEUE)
-    channel.basic_publish(exchange='',
-                          routing_key=settings.RABBIT_QUEUE,
-                          body=notification)
-    logger.debug(" [x] Queued notification to " + settings.RABBIT_QUEUE)
-    connection.close()
 
-@app.route('/responses', methods=['POST'])
-def do_save_response():
-    survey_response = request.get_json(force=True)
+def get_db_responses():
+    mongo_client = MongoClient(app.config['MONGODB_URL'])
+    return mongo_client.sdx_store.responses
+
+
+@app.errorhandler(400)
+def errorhandler_400(e):
+    return client_error(repr(e))
+
+
+def client_error(error=None):
+    logger.error(error, status=400)
+    message = {
+        'status': 400,
+        'message': error,
+        'uri': request.url
+    }
+    resp = jsonify(message)
+    resp.status_code = 400
+
+    return resp
+
+
+@app.errorhandler(500)
+def errorhandler_500(e):
+    return server_error(repr(e))
+
+
+@app.errorhandler(500)
+def server_error(error=None):
+    logger.error(error, status=500)
+    message = {
+        'status': 500,
+        'message': error
+    }
+    resp = jsonify(message)
+    resp.status_code = 500
+
+    return resp
+
+
+def queue_notification(notification, bound_logger):
+    bound_logger = bound_logger.bind(queue=settings.RABBIT_QUEUE, notification=notification)
+    bound_logger.debug("Queuing notification")
+    try:
+        connection = pika.BlockingConnection(pika.URLParameters(settings.RABBIT_URL))
+        channel = connection.channel()
+        channel.queue_declare(queue=settings.RABBIT_QUEUE)
+        channel.basic_publish(exchange='', routing_key=settings.RABBIT_QUEUE, body=notification)
+        bound_logger.debug("Queued notification")
+        connection.close()
+        return True
+
+    except Exception as e:
+        # TODO: how to deal with retry?
+        bound_logger.error("Unable to queue notification", exception=repr(e))
+        return False
+
+
+def save_response(survey_response, bound_logger):
     doc = {}
     doc['survey_response'] = survey_response
     doc['added_date'] = datetime.utcnow()
     try:
-        result = db.responses.insert_one( doc )
-        queue_notification(str(result.inserted_id))
+        result = get_db_responses().insert_one(doc)
+        return str(result.inserted_id)
+
     except pymongo.errors.OperationFailure as e:
-        return jsonify(error=str(e)), 400
+        bound_logger.error("Failed to store survey response", exception=str(e))
+        return None
+
+
+@app.route('/responses', methods=['POST'])
+def do_save_response():
+    survey_response = request.get_json(force=True)
+    metadata = survey_response['metadata']
+    bound_logger = logger.bind(user_id=metadata['user_id'], ru_ref=metadata['ru_ref'])
+
+    inserted_id = save_response(survey_response, bound_logger)
+    if inserted_id is None:
+        return server_error("Unable to save response")
+
+    queued = queue_notification(inserted_id, bound_logger)
+    if queued is False:
+        return server_error("Unable to queue notification")
 
     return jsonify(result="ok")
 
-schema = Schema({
-   'survey_id': str,
-   'form': str,
-   'ru_ref': str,
-   'period': str,
-   'added_ms': Coerce(int),
-   'per_page': All(Coerce(int), Range(min=1, max=100)),
-   'page': All(Coerce(int), Range(min=1)),
-})
 
 @app.route('/responses', methods=['GET'])
 def do_get_responses():
     try:
         schema(request.args)
     except MultipleInvalid as e:
-        return jsonify(error=str(e)), 400
+        logger.error("Request args failed schema validation", error=str(e))
+        return client_error(repr(e))
+
     survey_id = request.args.get('survey_id')
-    form      = request.args.get('form')
-    ru_ref    = request.args.get('ru_ref')
-    period    = request.args.get('period')
-    added_ms  = request.args.get('added_ms')
-    page      = request.args.get('page')
-    per_page  = request.args.get('per_page')
+    form = request.args.get('form')
+    ru_ref = request.args.get('ru_ref')
+    period = request.args.get('period')
+    added_ms = request.args.get('added_ms')
+    page = request.args.get('page')
+    per_page = request.args.get('per_page')
 
     # paging defaults
     if not page:
@@ -83,21 +142,31 @@ def do_get_responses():
         per_page = int(per_page)
 
     search_criteria = {}
-    if survey_id: search_criteria['survey_response.survey_id'] = survey_id
-    if form:      search_criteria['survey_response.form'] = form
-    if ru_ref:    search_criteria['survey_response.metadata.ru_ref'] = ru_ref
-    if period:    search_criteria['survey_response.collection.period'] = period
-    if added_ms:  search_criteria['added_date'] = { "$gte" : datetime.fromtimestamp(int(added_ms)/1000.0) }
+    if survey_id:
+        search_criteria['survey_response.survey_id'] = survey_id
+    if form:
+        search_criteria['survey_response.form'] = form
+    if ru_ref:
+        search_criteria['survey_response.metadata.ru_ref'] = ru_ref
+    if period:
+        search_criteria['survey_response.collection.period'] = period
+    if added_ms:
+        search_criteria['added_date'] = {
+            "$gte": datetime.fromtimestamp(int(added_ms) / 1000.0)
+        }
 
     results = {}
-    responses  = []
-    count = db.responses.find(search_criteria).count()
+    responses = []
+    db_responses = get_db_responses()
+    count = db_responses.find(search_criteria).count()
     results['total_hits'] = count
-    cursor = db.responses.find(search_criteria).skip(per_page*(page-1)).limit(per_page)
+    cursor = db_responses.find(search_criteria).skip(per_page * (page - 1)).limit(per_page)
     for document in cursor:
         document['_id'] = str(document['_id'])
-        document['added_ms'] = int(document['added_date'].strftime("%s")) * 1000
+        if added_ms:
+            document['added_ms'] = int(document['added_date'].strftime("%s")) * 1000
         responses.append(document)
+
     results['results'] = responses
     return jsonify(results)
 
@@ -105,21 +174,35 @@ def do_get_responses():
 @app.route('/responses/<mongo_id>', methods=['GET'])
 def do_get_response(mongo_id):
     try:
-        result = db.responses.find_one({"_id": ObjectId(mongo_id)})
-
+        result = get_db_responses().find_one({"_id": ObjectId(mongo_id)})
         if result:
             result['_id'] = str(result['_id'])
             return jsonify(result)
+
     except InvalidId as e:
-        return jsonify({}), 404
+        return client_error(repr(e))
+
     except Exception as e:
-        raise e
+        return server_error(repr(e))
 
     return jsonify({}), 404
 
 
+@app.route('/queue', methods=['POST'])
+def do_queue():
+    mongo_id = request.get_json(force=True)['id']
+    # check document exists with id
+    result = do_get_response(mongo_id)
+    if result.status_code != 200:
+        return result
+
+    queued = queue_notification(mongo_id, logger)
+    if queued is False:
+        return server_error("Unable to queue notification")
+
+    return jsonify(result="ok")
+
+
 if __name__ == '__main__':
-    # Startup
-    logging.basicConfig(level=settings.LOGGING_LEVEL, format=settings.LOGGING_FORMAT)
-    port = int(os.getenv("PORT"))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    logger.debug("START")
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ tests/test_data.py
@@ -0,0 +1,49 @@
+test_message = '''
+{
+  "type": "uk.gov.ons.edc.eq:surveyresponse",
+  "origin": "uk.gov.ons.edc.eq",
+  "survey_id": "194825",
+  "version": "0.0.1",
+  "collection": {
+    "exercise_sid": "hfjdskf",
+    "instrument_id": "10",
+    "period": "0616"
+  },
+  "submitted_at": "2016-03-12T10:39:40Z",
+  "metadata": {
+    "user_id": "789473423",
+    "ru_ref": "1234570071A"
+  },
+  "data": {
+    "1": "2",
+    "2": "4",
+    "3": "2",
+    "4": "Y"
+  }
+}
+'''
+
+updated_message = '''
+{
+  "type": "uk.gov.ons.edc.eq:surveyresponse",
+  "origin": "uk.gov.ons.edc.eq",
+  "survey_id": "194826",
+  "version": "0.0.1",
+  "collection": {
+    "exercise_sid": "hfjdskf",
+    "instrument_id": "10",
+    "period": "0617"
+  },
+  "submitted_at": "2016-03-12T10:39:40Z",
+  "metadata": {
+    "user_id": "789473423",
+    "ru_ref": "1234570081A"
+  },
+  "data": {
+    "1": "2",
+    "2": "4",
+    "3": "2",
+    "4": "Y"
+  }
+}
+'''
