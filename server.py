05da5ba64532ba985865a5a9ddd5bc99322a0aa2
Eleanor Deal <eleanor.deal@gmail.com>
2017-01-10 18:09:20 +0000

    Remove start log message

--- server.py
+++ server.py
@@ -240,7 +240,5 @@ def healthcheck():
 
 if __name__ == '__main__':
     # Startup
-    logger.debug("START")
-
     port = int(os.getenv("PORT"))
     app.run(debug=True, host='0.0.0.0', port=port)
