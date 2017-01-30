d8f5d0e455a1e09726303dd1d47d32cb433aa8ef
tundish <tundish@thuswise.org>
2017-01-30 15:43:09 +0000

    Added tests for pooled connection.

--- tests/test_pgstore.py
+++ tests/test_pgstore.py
@@ -1,5 +1,8 @@
+import os
 import unittest
 
+import psycopg2.extensions
+import psycopg2.pool
 import testing.postgresql
 
 from pgstore import ProcessSafePoolManager
@@ -14,6 +17,27 @@ class PoolManagerTests(unittest.TestCase):
     def tearDown(self):
         self.db.stop()
 
-    def test_connect(self):
+    def test_first_connect(self):
         pm = ProcessSafePoolManager(**self.db.dsn())
         self.assertIsNone(pm._pool)
+
+        con = pm.getconn()
+        self.assertIsInstance(pm._pool, psycopg2.pool.ThreadedConnectionPool)
+        self.assertEqual(1, len(pm._pool._used))
+        self.assertIsInstance(con, psycopg2.extensions.connection)
+
+        pm.putconn(con)
+        self.assertEqual(0, len(pm._pool._used))
+
+    def test_connect_after_fork(self):
+        pm = ProcessSafePoolManager(**self.db.dsn())
+        self.assertEqual(os.getpid(), pm.pidLastSeen)
+
+        con = pm.getconn()
+        pools = [pm._pool]
+
+        pm.pidLastSeen -= 1
+        con = pm.getconn()
+        pools.append(pm._pool)
+
+        self.assertIsNot(pools[0], pools[1])
