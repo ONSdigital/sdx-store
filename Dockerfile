95d0fd1024f503ac6cfea014c0592e8d0cb41645
Warren Bailey <warren@warrenbailey.net>
2017-04-21 09:10:49 +0100

    Renaming env variables to match postgres and updating docker file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM onsdigital/flask-crypto
+ENV BUILD_PACKAGES="curl build-essential python3-dev ca-certificates libssl-dev libffi-dev"
 
 ADD server.py /app/server.py
 ADD settings.py /app/settings.py
@@ -10,9 +11,12 @@ RUN mkdir -p /app/logs
 
 # set working directory to /app/
 WORKDIR /app/
+EXPOSE 5000
 
 EXPOSE 5000
 
+
+RUN apt-get update && apt-get install -y $BUILD_PACKAGES postgresql-9.4 libpq-dev
 RUN pip3 install --no-cache-dir -U -I -r /app/requirements.txt
 
 ENTRYPOINT ./startup.sh
--- settings.py
+++ settings.py
@@ -26,8 +26,8 @@ RABBIT_URL2 = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
 
 RABBIT_URLS = [RABBIT_URL, RABBIT_URL2]
 
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
-DB_PORT = os.getenv('DB_PORT', '15432')
-DB_NAME = os.getenv('DB_NAME', 'sdx')
-DB_USER = os.getenv('DB_USER', 'sdx')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'secret')
+DB_HOST = os.getenv('POSTGRES_HOST', '127.0.0.1')
+DB_PORT = os.getenv('POSTGRES_PORT', '15432')
+DB_NAME = os.getenv('POSTGRES_NAME', 'sdx')
+DB_USER = os.getenv('POSTGRES_USER', 'sdx')
+DB_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'secret')
