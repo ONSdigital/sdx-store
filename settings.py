9c5e7484d143a257d0857f401f170a26fefb9ca2
James Gardiner <james@jgardiner.co.uk>
2017-06-22 10:58:58 +0100

    Update settings for cf

--- settings.py
+++ settings.py
@@ -1,38 +1,18 @@
+import json
 import os
 import logging
 
 
 LOGGING_LEVEL = logging.getLevelName(os.getenv('LOGGING_LEVEL', 'DEBUG'))
 
-DB_HOST = os.getenv('SDX_STORE_POSTGRES_HOST', '0.0.0.0')
-DB_PORT = os.getenv('SDX_STORE_POSTGRES_PORT', '5432')
-DB_NAME = os.getenv('SDX_STORE_POSTGRES_NAME', 'postgres')
-DB_USER = os.getenv('SDX_STORE_POSTGRES_USER', 'postgres')
-DB_PASSWORD = os.getenv('SDX_STORE_POSTGRES_PASSWORD', 'secret')
-DB_URI = 'postgresql://{}:{}@{}:{}/{}'.format(DB_USER,
-                                              DB_PASSWORD,
-                                              DB_HOST,
-                                              DB_PORT,
-                                              DB_NAME)
+env_vars = os.getenv('VCAP_SERVICES')
+rmq = json.loads(env_vars)['rabbitmq'][0]
+RABBIT_URL = rmq['credentials']['protocols']['amqp']['uri']
 
+psql = json.loads(env_vars)['rds'][0]
+DB_URI = psql['credentials']['uri']
 
-RABBIT_URL = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
-    hostname=os.getenv('SDX_STORE_RABBITMQ_HOST', 'rabbit'),
-    port=os.getenv('SDX_STORE_RABBITMQ_PORT', '5672'),
-    user=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_USER', 'rabbit'),
-    password=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_PASS', 'rabbit'),
-    vhost=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_VHOST', '%2f')
-)
-
-RABBIT_URL2 = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
-    hostname=os.getenv('SDX_STORE_RABBITMQ_HOST2', 'rabbit'),
-    port=os.getenv('SDX_STORE_RABBITMQ_PORT2', '5433'),
-    user=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_USER', 'rabbit'),
-    password=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_PASS', 'rabbit'),
-    vhost=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_VHOST', '%2f')
-)
-
-RABBIT_URLS = [RABBIT_URL, RABBIT_URL2]
+RABBIT_URLS = [RABBIT_URL]
 
 RABBIT_CORA_QUEUE = os.getenv('SDX_STORE_RABBIT_CORA_QUEUE',
                               'sdx-cora-survey-notifications')
@@ -42,4 +22,4 @@ RABBIT_CS_QUEUE = os.getenv('SDX_STORE_RABBIT_CS_QUEUE',
                             'sdx-cs-survey-notifications')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS',
-                                           default=False)
+                                                                               default=False)
