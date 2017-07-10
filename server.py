59c387c19243e759eca0e66a6fdd22c8137dd560
Adam Mann <manny160389@hotmail.com>
2017-07-10 15:48:54 +0100

    Add missing logging (#56)
    Adds missing logging to enable traceability of transactions through logs

--- server.py
+++ server.py
@@ -232,15 +232,19 @@ def do_save_response():
     tx_id = survey_response['tx_id']
 
     if survey_response['survey_id'] == 'census':
+        bound_logger.info("About to publish notification to ctp queue")
         queued = publisher.ctp.publish_message(tx_id)
     elif survey_response['survey_id'] == '144':
+        bound_logger.info("About to publish notification to cora queue")
         queued = publisher.cora.publish_message(tx_id)
     else:
+        bound_logger.info("About to publish notification to cs queue")
         queued = publisher.cs.publish_message(tx_id)
 
     if not queued:
         return server_error("Unable to queue notification")
 
+    bound_logger.info("Notification published successfully")
     publisher.logger = logger
     return jsonify(result="ok")
 
@@ -290,16 +294,24 @@ def do_queue():
 
     response = json.loads(result.response[0].decode('utf-8'))
 
+    bound_logger = logger.bind(tx_id=tx_id)
+    publisher.logger = bound_logger
+
     if response['survey_response']['survey_id'] == 'census':
+        bound_logger.info("About to publish response to ctp queue")
         queued = publisher.ctp.publish_message(tx_id)
     elif response['survey_response']['survey_id'] == '144':
+        bound_logger.info("About to publish response to cora queue")
         queued = publisher.cora.publish_message(tx_id)
     else:
+        bound_logger.info("About to publish response to cs queue")
         queued = publisher.cs.publish_message(tx_id)
 
     if not queued:
         return server_error("Unable to queue response")
 
+    bound_logger.info("Response published successfully")
+    publisher.logger = logger
     return jsonify(result="ok")
 
 
