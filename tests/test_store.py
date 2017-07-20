3824ac5128cc880c2f597d14979ce94968751a43
Tejas Patel <tejas.patel@ons.gov.uk>
2017-07-20 09:26:45 +0100

    Remove skipping tests

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import unittest
-import os
 
 import mock
 from structlog import wrap_logger
@@ -35,55 +34,46 @@ class TestStoreService(unittest.TestCase):
     test_json = json.loads(test_message)
     updated_json = json.loads(updated_message)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def setUp(self):
         self.postgres = Postgresql()
         self.app = server.app.test_client()
         self.app.testing = True
         server.create_tables()
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.postgres.stop()
 
     # /responses POST
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_empty_post_request(self):
         r = self.app.post(self.endpoints['responses'])
         self.assertEqual(400, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_response_not_saved_raises_InvalidUsageError(self):
         with self.assertRaises(InvalidUsageError):
             server.save_response(self.logger, json.loads(missing_tx_id_message))
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_response_invalid_true_returns_false(self):
         invalid = server.save_response(logger, json.loads(invalid_message))
         self.assertEqual(True, invalid)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_feedback_response_invalid_true_returns_false(self):
         invalid = server.save_feedback_response(logger, json.loads(invalid_feedback_message))
         self.assertEqual(True, invalid)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_response_not_saved_returns_500(self):
         with mock.patch('server.db.session.commit') as db_mock:
             db_mock.side_effect = SQLAlchemyError
             r = self.app.post(self.endpoints['responses'], data=test_message)
             self.assertEqual(500, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_response_not_saved_returns_500_feedback(self):
         with mock.patch('server.db.session.commit') as db_mock:
             db_mock.side_effect = SQLAlchemyError
             r = self.app.post(self.endpoints['responses'], data=test_feedback_message)
             self.assertEqual(500, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_queue_fails_returns_500(self):
         with mock.patch('server.publisher.cs.publish_message', return_value=False):
             r = self.app.post(self.endpoints['responses'], data=test_message)
@@ -92,7 +82,6 @@ class TestStoreService(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_integrity_error_returns_500(self):
         with mock.patch('server.db.session.commit') as db_mock:
             db_mock.side_effect = IntegrityError(None, None, None, None)
@@ -102,7 +91,6 @@ class TestStoreService(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_integrity_error_returns_500_feedback(self):
         with mock.patch('server.db.session.commit') as db_mock:
             db_mock.side_effect = IntegrityError(None, None, None, None)
@@ -112,7 +100,6 @@ class TestStoreService(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_queue_succeeds_returns_200(self):
         with mock.patch('server.publisher.cs.publish_message', return_value=True):
             r = self.app.post(self.endpoints['responses'], data=test_message)
@@ -122,12 +109,10 @@ class TestStoreService(unittest.TestCase):
         db.drop_all()
 
     # /responses/<tx_id> GET
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_get_id_returns_404_if_not_stored(self):
         r = self.app.get(self.endpoints['responses'] + '/x')
         self.assertEqual(404, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_get_valid_id_returns_id_and_200(self):
         with mock.patch('server.publisher.cs.publish_message', return_value=True):
             test_json = json.loads(updated_message)
@@ -145,17 +130,14 @@ class TestStoreService(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_get_responses_invalid_params(self):
         r = self.app.get(self.endpoints['responses'] + '?testing=123')
         self.assertEqual(400, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_get_responses_valid_and_invalid_params(self):
         r = self.app.get(self.endpoints['responses'] + '?survey_id=123&testing=123')
         self.assertEqual(400, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_get_responses_per_page(self):
         with mock.patch('server.publisher.cs.publish_message', return_value=True):
             self.app.post(self.endpoints['responses'],
@@ -176,32 +158,26 @@ class TestStoreService(unittest.TestCase):
         db.drop_all()
 
     # test ranges for params
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_min_range_per_page(self):
         r = self.app.get(self.endpoints['responses'] + '?per_page=0')
         self.assertEqual(400, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_max_range_per_page(self):
         r = self.app.get(self.endpoints['responses'] + '?per_page=101')
         self.assertEqual(400, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_min_range_page_ok(self):
         r = self.app.get(self.endpoints['responses'] + '?page=1')
         self.assertEqual(200, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_min_range_page_bad(self):
         r = self.app.get(self.endpoints['responses'] + '?page=0')
         self.assertEqual(400, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_healthcheck_good(self):
         r = self.app.get(self.endpoints['healthcheck'])
         self.assertEqual(200, r.status_code)
 
-    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", "postgres issues on travis")
     def test_healthcheck_bad_returns_500(self):
         with mock.patch('server.test_sql') as healthMock:
             healthMock.side_effect = SQLAlchemyError
