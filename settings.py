490257e7209c05eb6e51d2a0755877caff6cca49
Tejas Patel <tejas.patel@ons.gov.uk>
2017-09-06 12:20:01 +0100

    Remove response notifications

--- settings.py
+++ settings.py
@@ -15,31 +15,5 @@ DB_URI = 'postgresql://{}:{}@{}:{}/{}'.format(DB_USER,
                                               DB_PORT,
                                               DB_NAME)
 
-
-RABBIT_URL = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
-    hostname=os.getenv('SDX_STORE_RABBITMQ_HOST', 'rabbit'),
-    port=os.getenv('SDX_STORE_RABBITMQ_PORT', '5672'),
-    user=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_USER', 'rabbit'),
-    password=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_PASS', 'rabbit'),
-    vhost=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_VHOST', '%2f')
-)
-
-RABBIT_URL2 = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
-    hostname=os.getenv('SDX_STORE_RABBITMQ_HOST2', 'rabbit'),
-    port=os.getenv('SDX_STORE_RABBITMQ_PORT2', '5433'),
-    user=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_USER', 'rabbit'),
-    password=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_PASS', 'rabbit'),
-    vhost=os.getenv('SDX_STORE_RABBITMQ_DEFAULT_VHOST', '%2f')
-)
-
-RABBIT_URLS = [RABBIT_URL, RABBIT_URL2]
-
-RABBIT_CORA_QUEUE = os.getenv('SDX_STORE_RABBIT_CORA_QUEUE',
-                              'sdx-cora-survey-notifications')
-RABBIT_CTP_QUEUE = os.getenv('SDX_STORE_RABBIT_CTP_QUEUE',
-                             'sdx-ctp-survey-notifications')
-RABBIT_CS_QUEUE = os.getenv('SDX_STORE_RABBIT_CS_QUEUE',
-                            'sdx-cs-survey-notifications')
-
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS',
                                            default=False)
