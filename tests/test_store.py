47174fc4b493f77e64599610c06f47ead9bd2104
Adam Mann <manny160389@hotmail.com>
2019-09-16 15:20:56 +0100

    Update packages and improve tests

--- tests/test_store.py
+++ tests/test_store.py
@@ -17,6 +17,18 @@ from server import db, InvalidUsageError, logger
 
 @testing.postgresql.skipIfNotInstalled
 class TestStoreService(unittest.TestCase):
+    """There are a number of tests of failure states that have a commented out assertEquals in them with
+    different longer assertion then what isn't commented out.  The reason for this is that the return value
+    changes depending on what python version is being used (No I don't know why...).
+
+    In python 3.6.3 it returns the param name as a key, and the value is a list.  In python 3.6.7+ and 3.7+
+    the key is the same, but the value is a single value instead of a list.  Because we want to test both 3.6 and
+    3.7 in travis, we've opted to test the common values between them until we upgrade the python version so we
+    can test the whole response.
+
+    The aim of leaving the tests commented out is so after the version is upgraded to 3.6.7+, they should be uncommented
+    the weaker test above it removed and everything should just work.
+    """
     endpoints = {
         'responses': '/responses',
         'invalid': '/invalid-responses',
@@ -44,7 +56,7 @@ class TestStoreService(unittest.TestCase):
     # /responses POST
     def test_empty_post_request(self):
         r = self.app.post(self.endpoints['responses'])
-        self.assertEqual(400, r.status_code)
+        self.assertEqual(r.status_code, 400)
 
     def test_response_not_saved_raises_InvalidUsageError(self):
         with self.assertRaises(InvalidUsageError):
@@ -52,29 +64,29 @@ class TestStoreService(unittest.TestCase):
 
     def test_response_invalid_true_returns_false(self):
         invalid = server.save_response(logger, json.loads(invalid_message))
-        self.assertEqual(True, invalid)
+        self.assertTrue(invalid)
 
     def test_feedback_response_invalid_true_returns_false(self):
         invalid = server.save_feedback_response(logger, json.loads(invalid_feedback_message))
-        self.assertEqual(True, invalid)
+        self.assertTrue(invalid)
 
     def test_response_not_saved_returns_500(self):
         with mock.patch('server.db.session.commit') as db_mock:
             db_mock.side_effect = SQLAlchemyError
             r = self.app.post(self.endpoints['responses'], data=test_message)
-            self.assertEqual(500, r.status_code)
+            self.assertEqual(r.status_code, 500)
 
     def test_response_not_saved_returns_500_feedback(self):
         with mock.patch('server.db.session.commit') as db_mock:
             db_mock.side_effect = SQLAlchemyError
             r = self.app.post(self.endpoints['responses'], data=test_feedback_message)
-            self.assertEqual(500, r.status_code)
+            self.assertEqual(r.status_code, 500)
 
     def test_integrity_error_returns_500(self):
         with mock.patch('server.db.session.commit') as db_mock:
             db_mock.side_effect = IntegrityError(None, None, None, None)
             r = self.app.post(self.endpoints['responses'], data=test_message)
-            self.assertEqual(500, r.status_code)
+            self.assertEqual(r.status_code, 500)
 
         db.session.remove()
         db.drop_all()
@@ -83,7 +95,7 @@ class TestStoreService(unittest.TestCase):
         with mock.patch('server.db.session.commit') as db_mock:
             db_mock.side_effect = IntegrityError(None, None, None, None)
             r = self.app.post(self.endpoints['responses'], data=test_feedback_message)
-            self.assertEqual(500, r.status_code)
+            self.assertEqual(r.status_code, 500)
 
         db.session.remove()
         db.drop_all()
@@ -127,7 +139,7 @@ class TestStoreService(unittest.TestCase):
         r = self.app.get(self.endpoints['responses'] + '/' + expected_id)
 
         self.assertEqual(r.data, self.test_message_sorted.encode('utf-8'))
-        self.assertEqual(200, r.status_code)
+        self.assertEqual(r.status_code, 200)
         self.assertEqual(r.headers['Content-MD5'], response_hash_original)
         self.assertEqual(r.headers['Content-Length'], str(response_size_original))
 
@@ -135,12 +147,18 @@ class TestStoreService(unittest.TestCase):
         db.drop_all()
 
     def test_get_responses_invalid_params(self):
+        """Endpoint should return 400 if given an invalid parameter"""
         r = self.app.get(self.endpoints['responses'] + '?testing=123')
-        self.assertEqual(400, r.status_code)
+        self.assertEqual(r.status_code, 400)
+        self.assertEqual(r.json['message'], 'Request args failed schema validation')
+        # self.assertEqual(r.json, {'message': 'Request args failed schema validation', 'testing': '123'})
 
     def test_get_responses_valid_and_invalid_params(self):
+        """Endpiont should return 400 if given an invalid parameter, even if there are valid ones present"""
         r = self.app.get(self.endpoints['responses'] + '?survey_id=123&testing=123')
-        self.assertEqual(400, r.status_code)
+        self.assertEqual(r.status_code, 400)
+        self.assertEqual(r.json['message'], 'Request args failed schema validation')
+        # self.assertEqual(r.json, {'message': 'Request args failed schema validation', 'survey_id': '123', 'testing': '123'})
 
     def test_get_responses_per_page(self):
         self.app.post(self.endpoints['responses'],
@@ -162,27 +180,47 @@ class TestStoreService(unittest.TestCase):
 
     # test ranges for params
     def test_min_range_per_page(self):
+        """Endpoint should return 400 if a parameter fails schema validation, in this case
+        being below the minimum value for 'per_page'
+        """
         r = self.app.get(self.endpoints['responses'] + '?per_page=0')
-        self.assertEqual(400, r.status_code)
+        self.assertEqual(r.status_code, 400)
+        self.assertEqual(r.json['message'], 'Request args failed schema validation')
+        # self.assertEqual(r.json, {'message': 'Request args failed schema validation', 'per_page': '0'})
 
     def test_max_range_per_page(self):
+        """Endpoint should return 400 if a parameter fails schema validation, in this case
+        being below the maximumvalue for 'per_page'
+        """
         r = self.app.get(self.endpoints['responses'] + '?per_page=101')
-        self.assertEqual(400, r.status_code)
+        self.assertEqual(r.status_code, 400)
+        self.assertEqual(r.json['message'], 'Request args failed schema validation')
+        # self.assertEqual(r.json, {'message': 'Request args failed schema validation', 'per_page': '101'})
 
     def test_min_range_page_ok(self):
+        """Endpoint should return 200 if parameters pass schema validation"""
         r = self.app.get(self.endpoints['responses'] + '?page=1')
-        self.assertEqual(200, r.status_code)
+        self.assertEqual(r.status_code, 200)
 
     def test_min_range_page_bad(self):
+        """Endpoint should return 400 if a parameter fails schema validation, in this case
+        being below the minimum value for 'page'
+        """
         r = self.app.get(self.endpoints['responses'] + '?page=0')
-        self.assertEqual(400, r.status_code)
+        self.assertEqual(r.status_code, 400)
+        self.assertEqual(r.json['message'], 'Request args failed schema validation')
+        # self.assertEqual(r.json, {'message': 'Request args failed schema validation', 'page': '0'})
 
     def test_healthcheck_good(self):
+        """Healthcheck endpoint should return 200"""
         r = self.app.get(self.endpoints['healthcheck'])
-        self.assertEqual(200, r.status_code)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json, {'status': 'OK'})
 
     def test_healthcheck_bad_returns_500(self):
-        with mock.patch('server.test_sql') as healthMock:
-            healthMock.side_effect = SQLAlchemyError
+        """Healthcheck endpoint should return 500 on failure with a message as to why it's failed"""
+        with mock.patch('server.test_sql') as health_mock:
+            health_mock.side_effect = SQLAlchemyError
             r = self.app.get(self.endpoints['healthcheck'])
-            self.assertEqual(500, r.status_code)
+            self.assertEqual(r.status_code, 500)
+            self.assertEqual(r.json, {'message': 'Failed to connect to database', 'status': 500})
