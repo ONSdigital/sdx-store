802e3e31aca1eec4254276d776de7d91396669f9
Adam Mann <manny160389@hotmail.com>
2020-05-05 13:00:07 +0100

    Remove cloudfoundry files (#137)

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn --timeout=60 --workers=8 --threads=8 server:app
--- settings.py
+++ settings.py
@@ -1,29 +1,16 @@
 import os
 import logging
-import json
 
 
 LOGGING_LEVEL = logging.getLevelName(os.getenv('LOGGING_LEVEL', 'DEBUG'))
 LOGGING_FORMAT = "%(asctime)s.%(msecs)06dZ|%(levelname)s: sdx-store: %(message)s"
 
-if os.getenv("CF_DEPLOYMENT", False):
-    vcap_services = os.getenv("VCAP_SERVICES")
-    parsed_vcap_services = json.loads(vcap_services)
-    rds_config = parsed_vcap_services.get('rds')
-    DB_URI = rds_config[0].get('credentials').get('uri')
-
-else:
-    DB_HOST = os.getenv('SDX_STORE_POSTGRES_HOST', 'localhost')
-    DB_PORT = os.getenv('SDX_STORE_POSTGRES_PORT', '5433')
-    DB_NAME = os.getenv('SDX_STORE_POSTGRES_NAME', 'sdx')
-    DB_USER = os.getenv('SDX_STORE_POSTGRES_USER', 'sdx')
-    DB_PASSWORD = os.getenv('SDX_STORE_POSTGRES_PASSWORD', 'sdx')
-    DB_URI = 'postgresql://{}:{}@{}:{}/{}'.format(DB_USER,
-                                                  DB_PASSWORD,
-                                                  DB_HOST,
-                                                  DB_PORT,
-                                                  DB_NAME)
+DB_HOST = os.getenv('SDX_STORE_POSTGRES_HOST', 'localhost')
+DB_PORT = os.getenv('SDX_STORE_POSTGRES_PORT', '5433')
+DB_NAME = os.getenv('SDX_STORE_POSTGRES_NAME', 'sdx')
+DB_USER = os.getenv('SDX_STORE_POSTGRES_USER', 'sdx')
+DB_PASSWORD = os.getenv('SDX_STORE_POSTGRES_PASSWORD', 'sdx')
+DB_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 RESPONSE_RETENTION_DAYS = os.getenv('SDX_STORE_RESPONSE_RETENTION_DAYS')  # No default
-
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', default=False)
