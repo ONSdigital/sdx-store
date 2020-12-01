2e2943f0866fe3a80d8b37f2182886e7197bb3b5
bkhan2 <mohammed.khan@ons.gov.uk>
2020-12-01 15:06:13 +0000

    updating version no. in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from structlog import wrap_logger
 
 import settings
 
-__version__ = "3.14.0"
+__version__ = "3.15.0"
 
 logging.basicConfig(format=settings.LOGGING_FORMAT,
                     datefmt="%Y-%m-%dT%H:%M:%S",
